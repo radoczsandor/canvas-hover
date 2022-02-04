cda586aecbf5fb428b953b49ef42acb4bbb8ff0e
radoczsandor <radocz.sandor1231@gmail.com>
2022-02-04 16:34:54 +0100

    items with coordinates

---
+++ javascript/items.js
@@ -0,0 +1,10 @@
+export const items = [
+    { id: 0, Name: 'item0', x: 20, y: 20, w: 60, h: 60 },
+    { id: 1, Name: 'item1', x: 20, y: 100, w: 60, h: 60 },
+    { id: 2, Name: 'item2', x: 90, y: 60, w: 60, h: 60 },
+    { id: 3, Name: 'item3', x: 160, y: 20, w: 60, h: 60 },
+    { id: 4, Name: 'item4', x: 160, y: 100, w: 60, h: 60 },
+    { id: 5, Name: 'item5', x: 230, y: 60, w: 60, h: 60 },
+    { id: 6, Name: 'item6', x: 300, y: 20, w: 60, h: 60 },
+    { id: 7, Name: 'item7', x: 300, y: 100, w: 60, h: 60 }
+];
